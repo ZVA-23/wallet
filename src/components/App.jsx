5b693d748ea8ba615e6f904c8dc7858ae72d3a27
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-11-12 12:44:32 +0200

    added routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,44 @@
-//import { useEffect } from 'react';
-//import { useDispatch } from 'react-redux';
-//import { refreshUser } from 'redux/auth/operations';
-import { LoginForm } from './LoginForm/LoginForm';
-import { RegisterForm } from './RegistrationForm/RegistrationForm';
+import { lazy, Suspense, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { refreshUser } from 'redux/auth/operations';
+import { selectAuth } from 'redux/selectors';
+import { getTransactionCategories } from 'redux/transactions/operations';
+import { PrivateRoute } from './PrivateRoute';
+import { RestrictedRoute } from './RestrictedRoute';
 
-
+const DashboardPage = lazy(() => import('pages/DashboardPage/DashboardPage'));
+const RegistrationPage = lazy(() => import('pages/RegistrationPage/RegistrationPage'));
+const LoginPage = lazy(() => import('pages/LoginPage/LoginPage'));
 
 export const App = () => {
-//const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const { isRefreshing } = useSelector(selectAuth);
 
+  useEffect(() => {
+    dispatch(refreshUser());
+    dispatch(getTransactionCategories());
+  }, [dispatch]);
 
+  if (isRefreshing) return <div className="text-center">Refreshing user...</div>;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <RegisterForm/>
-      <LoginForm/>
-    </div>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" replace />} />
+        <Route
+          path="/register"
+          element={<RestrictedRoute redirectTo="/dashboard" component={<RegistrationPage />} />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute redirectTo="/dashboard" component={<LoginPage />} />}
+        />
+        <Route
+          path="/dashboard"
+          element={<PrivateRoute redirectTo="/login" component={<DashboardPage />} />}
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Suspense>
   );
 };
