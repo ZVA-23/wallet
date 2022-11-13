d54c26fcc136536ea6b87d95cfdf7fb06d6a5a4c
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-11-13 14:17:52 +0200

    modal logout is added

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -1,9 +1,22 @@
+import { useDispatch } from "react-redux";
+import { logOut } from "redux/auth/operations";
 import { ModalLogoutWrapper, Text, LogoutBtn, CancelBtn } from "./ModalLogout.styled"
 
-export const ModalLogout = () => {
+export const ModalLogout = ({ showModalHandler }) => {
+	const dispatch = useDispatch();
+
+	const onClose = () => {
+		showModalHandler()
+	}
+
+	const handlerLogOut = () => {
+		dispatch(logOut());
+		showModalHandler();
+	}
+
 	return <ModalLogoutWrapper>
 		<Text>Do you want to log out?</Text>
-		<LogoutBtn type="button">Log out</LogoutBtn>
-		<CancelBtn type="button">Cancel</CancelBtn>
+		<LogoutBtn type="button" onClick={handlerLogOut}>Log out</LogoutBtn>
+		<CancelBtn type="button" onClick={onClose}>Cancel</CancelBtn>
 	</ModalLogoutWrapper>
 }
