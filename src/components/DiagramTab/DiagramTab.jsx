2e5f86e7899eeaa64727f44f2ca881f631fa9911
Vova Boychyshyn <93547207+qsunn@users.noreply.github.com>
2022-11-17 14:22:26 +0200

    fixed charts

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,67 +1,53 @@
-import React, { useState } from 'react';
-// import React, { useState, useEffect } from 'react';
-// import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-// import { useSelector } from 'react-redux';
-import { selectTransactions, selectTransactionsSummary } from 'redux/selectors';
-// import { getAllTransactions, getTransactionCategories, getTransactionsSummary } from 'redux/transactions/operations';
-import { getTransactionsSummary } from 'redux/transactions/operations';
-import { Box, BoxChart, BoxTitle, Title, BoxSelect, Inner, Balance } from './DiagramTab.styled';
-import { Chart } from "components/Chart/Chart";
-import { Table } from "components/Table/Table";
+import { useSelector } from 'react-redux';
+import { selectTransactions } from 'redux/selectors';
+import {
+  Box,
+  BoxChart,
+  BoxTitle,
+  Title,
+  BoxSelect,
+  Inner,
+  Balance,
+} from './DiagramTab.styled';
+import { Chart } from 'components/Chart/Chart';
+import { Table } from 'components/Table/Table';
 import { SelectForm } from 'components/SelectForm/SelectForm';
-// import { toast } from "react-toastify";
-// import 'react-toastify/dist/ReactToastify.css';
-import { Loader } from "components/Loader/Loader";
+
+import { Loader } from 'components/Loader/Loader';
 
 export const DiagramTab = () => {
+  const transactions = useSelector(selectTransactions);
 
-	const dispatch = useDispatch();
-	const transactions = useSelector(selectTransactions);
-	const transactionsSummary = useSelector(selectTransactionsSummary);
+  const totalBalance = transactions.totalBalance.toFixed(2);
 
-	const totalBalance = transactions.totalBalance.toFixed(2);
- 	
-  console.log(transactions);
-	// const [numberMonth, setNumberMonth] = useState(11);
-	// const [numberYear, setNumberYear] = useState(2022);
-	const [statisticsData, setStatisticsData] = useState({ ...transactionsSummary });
-	const isLoading = useSelector(selectTransactions).isLoading;
+  const isLoading = useSelector(selectTransactions).isLoading;
+  const { summary } = useSelector(selectTransactions);
 
-	const selectedHandler = ({ selectedMonth, selectedYear }) => {
+  if (isLoading) return <Loader />;
 
-		const month = parseInt(selectedMonth);
-		const year = parseInt(selectedYear);
-		console.log(selectedMonth);
-		console.log(selectedYear);
-		console.log(month);
-		console.log(year);
-
-		dispatch(getTransactionsSummary({ month, year}));
-		setStatisticsData({ ...transactionsSummary });
-	}
-
-	return (
-		<>
-			{isLoading && <Loader />}
-			<Box>
-				{!isLoading &&
-					(<><BoxTitle>
-						<Title>Statistics</Title>
-						<BoxChart>
-							<Chart statisticsData={statisticsData} />
-							{statisticsData.categoriesSummary.length > 0 &&
-								<Balance>{`₴ ${totalBalance}`}</Balance>}
-						</BoxChart>
-					</BoxTitle>
-						<Inner>
-							<BoxSelect>
-								<SelectForm onSelected={selectedHandler} />
-							</BoxSelect>
-							<Table statisticsData={statisticsData} />
-						</Inner>
-					</>)}
-			</Box>
-		</>
-	);
+  return (
+    <>
+      <Box>
+        {summary && (
+          <>
+            <BoxTitle>
+              <Title>Statistics</Title>
+              <BoxChart>
+                <Chart statisticsData={summary} />
+                {summary.categoriesSummary.length > 0 && (
+                  <Balance>{`₴ ${totalBalance}`}</Balance>
+                )}
+              </BoxChart>
+            </BoxTitle>
+            <Inner>
+              <BoxSelect>
+                <SelectForm />
+              </BoxSelect>
+              <Table statisticsData={summary} />
+            </Inner>
+          </>
+        )}
+      </Box>
+    </>
+  );
 };
