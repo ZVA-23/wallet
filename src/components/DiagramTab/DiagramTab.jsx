f1046761964e5da40159b899c6d51c8fff1151c4
Vitalii Boiko <107396176+vitaliiseeker@users.noreply.github.com>
2022-11-17 17:17:35 +0200

    add

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -1,53 +1,57 @@
-import { useSelector } from 'react-redux';
-import { selectTransactions } from 'redux/selectors';
-import {
-  Box,
-  BoxChart,
-  BoxTitle,
-  Title,
-  BoxSelect,
-  Inner,
-  Balance,
-} from './DiagramTab.styled';
-import { Chart } from 'components/Chart/Chart';
-import { Table } from 'components/Table/Table';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectTransactions, selectTransactionsSummary } from 'redux/selectors';
+import { getTransactionsSummary } from 'redux/transactions/operations';
+import { Box, BoxChart, BoxTitle, Title, BoxSelect, Inner, Balance } from './DiagramTab.styled';
+import { Chart } from "components/Chart/Chart";
+import { Table } from "components/Table/Table";
 import { SelectForm } from 'components/SelectForm/SelectForm';
-
-import { Loader } from 'components/Loader/Loader';
+import { Loader } from "components/Loader/Loader";
 
 export const DiagramTab = () => {
-  const transactions = useSelector(selectTransactions);
 
-  const totalBalance = transactions.totalBalance.toFixed(2);
+	const dispatch = useDispatch();
+	const transactions = useSelector(selectTransactions);
+	const transactionsSummary = useSelector(selectTransactionsSummary);
 
-  const isLoading = useSelector(selectTransactions).isLoading;
-  const { summary } = useSelector(selectTransactions);
+	const totalBalance = transactions.totalBalance.toFixed(2);
 
-  if (isLoading) return <Loader />;
+	const [numberMonth, setNumberMonth] = useState(new Date().getMonth()+1);
+	const [numberYear, setNumberYear] = useState(new Date().getFullYear());
+	const isLoading = useSelector(selectTransactions).isLoading;
 
-  return (
-    <>
-      <Box>
-        {summary && (
-          <>
-            <BoxTitle>
-              <Title>Statistics</Title>
-              <BoxChart>
-                <Chart statisticsData={summary} />
-                {summary.categoriesSummary.length > 0 && (
-                  <Balance>{`₴ ${totalBalance}`}</Balance>
-                )}
-              </BoxChart>
-            </BoxTitle>
-            <Inner>
-              <BoxSelect>
-                <SelectForm />
-              </BoxSelect>
-              <Table statisticsData={summary} />
-            </Inner>
-          </>
-        )}
-      </Box>
-    </>
-  );
+	useEffect(() => {
+		if (numberMonth && numberYear) {
+			dispatch(getTransactionsSummary({ month: numberMonth, year: numberYear }));
+		}
+	}, [numberMonth, numberYear, dispatch]);
+
+	return (
+		<>
+			{isLoading && <Loader />}
+			<Box>
+				{!isLoading &&
+					(<><BoxTitle>
+						<Title>Statistics</Title>
+						<BoxChart>
+							<Chart />
+							{transactionsSummary.categoriesSummary.length > 0 &&
+								<Balance>{`₴ ${totalBalance}`}</Balance>}
+						</BoxChart>
+					</BoxTitle>
+						<Inner>
+							<BoxSelect>
+								<SelectForm
+									numberMonth={numberMonth}
+									numberYear={numberYear}
+									setNumberMonth={setNumberMonth}
+									setNumberYear={setNumberYear}
+								/>
+							</BoxSelect>
+							<Table />
+						</Inner>
+					</>)}
+			</Box>
+		</>
+	);
 };
