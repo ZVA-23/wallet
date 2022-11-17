dc7ec9a9b74331f77a3289c1003ad9ccf0a5ba9e
Vitalii Boiko <107396176+vitaliiseeker@users.noreply.github.com>
2022-11-17 11:36:49 +0200

    added code

--- src/components/DiagramTab/DiagramTab.jsx
+++ src/components/DiagramTab/DiagramTab.jsx
@@ -5,13 +5,14 @@ import { useSelector, useDispatch } from 'react-redux';
 // import { useSelector } from 'react-redux';
 import { selectTransactions, selectTransactionsSummary } from 'redux/selectors';
 // import { getAllTransactions, getTransactionCategories, getTransactionsSummary } from 'redux/transactions/operations';
-import { getAllTransactions, getTransactionsSummary } from 'redux/transactions/operations';
+import { getTransactionsSummary } from 'redux/transactions/operations';
 import { Box, BoxChart, BoxTitle, Title, BoxSelect, Inner, Balance } from './DiagramTab.styled';
 import { Chart } from "components/Chart/Chart";
 import { Table } from "components/Table/Table";
 import { SelectForm } from 'components/SelectForm/SelectForm';
 // import { toast } from "react-toastify";
 // import 'react-toastify/dist/ReactToastify.css';
+import { Loader } from "components/Loader/Loader";
 
 export const DiagramTab = () => {
 
@@ -20,46 +21,46 @@ export const DiagramTab = () => {
 	const transactionsSummary = useSelector(selectTransactionsSummary);
 
 	const totalBalance = transactions.totalBalance.toFixed(2);
-
+ 	
+  console.log(transactions);
 	// const [numberMonth, setNumberMonth] = useState(11);
 	// const [numberYear, setNumberYear] = useState(2022);
 	const [statisticsData, setStatisticsData] = useState({ ...transactionsSummary });
+	const isLoading = useSelector(selectTransactions).isLoading;
 
-	// useEffect(() => {
-	// 	// dispatch(getAllTransactions());
-	// 	console.log(transactionsSummary);
-	// 	console.log(statisticsData);
-	// }, [dispatch]);
-
-	const submitHandler = ({ selectedMonth, selectedYear }) => {
-		// setNumberMonth(parseInt(selectedMonth));
-		// setNumberYear(parseInt(selectedYear));
+	const selectedHandler = ({ selectedMonth, selectedYear }) => {
 
 		const month = parseInt(selectedMonth);
 		const year = parseInt(selectedYear);
+		console.log(selectedMonth);
+		console.log(selectedYear);
+		console.log(month);
+		console.log(year);
 
-		dispatch(getTransactionsSummary({ month, year }));
-		dispatch(getAllTransactions());
+		dispatch(getTransactionsSummary({ month, year}));
 		setStatisticsData({ ...transactionsSummary });
 	}
 
 	return (
 		<>
+			{isLoading && <Loader />}
 			<Box>
-				<BoxTitle>
-					<Title>Statistics</Title>
-					<BoxChart>
-						<Chart statisticsData={statisticsData} />
-						{statisticsData.categoriesSummary.length > 0 &&
-							<Balance>{`₴ ${totalBalance}`}</Balance>}
-					</BoxChart>
-				</BoxTitle>
-				<Inner>
-					<BoxSelect>
-						<SelectForm onSubmit={submitHandler} />
-					</BoxSelect>
-					<Table statisticsData={statisticsData} />
-				</Inner>
+				{!isLoading &&
+					(<><BoxTitle>
+						<Title>Statistics</Title>
+						<BoxChart>
+							<Chart statisticsData={statisticsData} />
+							{statisticsData.categoriesSummary.length > 0 &&
+								<Balance>{`₴ ${totalBalance}`}</Balance>}
+						</BoxChart>
+					</BoxTitle>
+						<Inner>
+							<BoxSelect>
+								<SelectForm onSelected={selectedHandler} />
+							</BoxSelect>
+							<Table statisticsData={statisticsData} />
+						</Inner>
+					</>)}
 			</Box>
 		</>
 	);
